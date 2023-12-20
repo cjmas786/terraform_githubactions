a4953632809b6a8d46bddf4f924d70ef003f1e64
cjmas786 <130229433+cjmas786@users.noreply.github.com>
2023-12-20 18:44:09 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "AWS_REGION" {    
+    default = "eu-west-2"
+}
+
+
+provider "aws" {
+    region = "${var.AWS_REGION}"
+}
+
+
+resource "aws_vpc" "main" {
+ cidr_block = "30.0.0.0/16"
+ 
+ tags = {
+   Name = "Project VPC"
+ }
+}
+
